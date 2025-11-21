82d3e3c470de4a1c281a818893606286cc3a98e3
osricgjacobs <osricgjacobs@gmail.com>
2025-11-21 21:10:53 +0200

    basic react1

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Card({ title }) {
+  const [hasLiked, setHasLiked] = useState(false);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="card">
+      <h2>{title}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "Liked" : "Like"}
+      </button>
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <div>
+      <h2>Functional Component</h2>
+
+      <Card title="Star wars" />
+      <Card title="Batman" />
+      <Card title="Dragon Ball" />
+    </div>
+  );
+}
+export default App;
